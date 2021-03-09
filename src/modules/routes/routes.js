c21f4fa0ec9f49d841cde9ccd36bc7ac5597f3ed
user <dimasurdin@yandex.ru>
2021-03-09 19:03:43 +0300

    Start doing the main page

--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -2,13 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  getAllUsers,
   createUser,
   loginUser,
 } = require('../controllers/user.controller');
 
-// router.get('/allUsers', getAllUsers);
+const {
+  getAllAppointments,
+  createAppointment,
+  deleteAppointment
+} = require('../controllers/appointment.controller');
+
 router.post('/createUser', createUser);
 router.post('/loginUser', loginUser);
+
+router.get('/allAppointments', getAllAppointments);
+router.post('/createAppointment', createAppointment);
+router.delete('/deleteAppointment', deleteAppointment);
 
 module.exports = router;
